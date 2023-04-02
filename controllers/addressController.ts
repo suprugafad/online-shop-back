f429879ac63e05cc76a6c507d51fd8f986a45ed7
suprugafad <poidaalesya382@gmail.com>
2023-04-02 20:13:25 +0300

    Change error message in addressController

--- controllers/addressController.ts
+++ controllers/addressController.ts
@@ -110,7 +110,7 @@ class AddressController {
       res.status(200).json(addresses);
     } catch (err) {
       console.error(err);
-      res.status(500).json({ message: 'Error getting addresses by country.' });
+      res.status(500).json({ message: 'Error getting addresses by city.' });
     }
   };
 
